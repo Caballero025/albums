f5f7e9393c5603df8ad012411649a6da65b95151
Caballero025 <l20590234@sjuanrio.tecnm.mx>
2025-12-13 20:37:01 -0600

    feat: agregar creación de álbumes

--- app/routes/album.py
+++ app/routes/album.py
@@ -12,3 +12,21 @@ def index():
     albums = Albums.query.all()
     return render_template('index.html', albums = albums)
 
+@alb_bp.route('/new',methods=['GET','POST'])
+def create_album():
+     if request.method == 'POST':
+         #Agregar Alumno 
+         id = request.form['id']
+         titulo = request.form['titulo']
+         artista = request.form['artista']
+         fecha_lanzamiento = request.form['fecha_lanzamiento']
+         genero = request.form['genero']
+         descripcion = request.form['descripcion']
+
+         nvo_album = Albums(id = id, titulo = titulo,artista = artista, fecha_lanzamiento = fecha_lanzamiento, genero = genero, descripcion = descripcion)
+
+         db.session.add(nvo_album)
+         db.session.commit()
+         return redirect(url_for('albums.index'))
+     #Aqui sigue si es GET
+     return render_template('create_album.html')
